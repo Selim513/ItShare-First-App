a46c6f8cef3e374afb3f4f988dcc648d2b8e1c8a
Selim513 <ahmedshaabanselem@gmail.com>
2023-12-20 20:02:12 +0200

    Final code

--- lib/new_screen.dart
+++ lib/new_screen.dart
@@ -1,30 +1,179 @@
+
+
 import 'package:flutter/material.dart';
 
 class NewScreen extends StatelessWidget {
+  const NewScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        leading: Icon(
-          Icons.menu,
-          size: 30,
-          color: Colors.white,
-        ),
         backgroundColor: Colors.black,
-        title: Text(
-          "Wleocme ",
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-        ),
-        centerTitle: true,
-        actions: [
-          Icon(
-            Icons.search,
-            color: Colors.white,
+        appBar: AppBar(
+          leading: const Icon(
+            Icons.menu,
             size: 30,
-          )
-        ],
-      ),
-    );
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.black,
+          title: const Text(
+            "Wleocme ",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          centerTitle: true,
+          
+          actions: const [
+            
+            Icon(
+              
+
+              Icons.search,
+              color: Colors.white,
+              size: 30,
+            ),
+          ],
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.black,
+                radius: 70,
+                child: Icon(
+                  Icons.person_outline_outlined,
+                  size: 100,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "Ahmed Selem",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Fresh Flutter Developer",
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.facebook,
+                    color: Colors.blue,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 100,
+                  ),
+                  Text(
+                    "Ahmed Shaaban",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(
+                    width: 70,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.blue,
+                    size: 30,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.camera_alt_outlined,
+                    color: Colors.red,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 90,
+                  ),
+                  Text(
+                    "@ahmedselem",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(
+                    width: 90,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.blue,
+                    size: 30,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.tiktok,
+                    color: Colors.grey,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 120,
+                  ),
+                  Text(
+                    "@Selem",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(
+                    width: 120,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.blue,
+                    size: 30,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.snapchat,
+                    color: Colors.yellow,
+                    size: 30,
+                  ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Text(
+                    "@AhmeD Selem",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  SizedBox(
+                    width: 80,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.blue,
+                    size: 30,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
